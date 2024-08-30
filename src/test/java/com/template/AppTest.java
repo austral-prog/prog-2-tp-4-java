011a76b4319047c46aace99d96d80da656fba5cd
mati <matias@keeper.app>
2024-08-30 09:50:56 -0300

    added elevator tests

--- src/test/java/com/template/AppTest.java
+++ src/test/java/com/template/AppTest.java
@@ -3,11 +3,44 @@ package com.template;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class AppTest {
+class ElevatorTest {
 
     @Test
-    void testAdd() {
-        App app = new App();
-        assertEquals(5, app.add(2, 3));
+    void testMoveToFloor() {
+        Elevator elevator = new Elevator(0, 10, 10);
+        elevator.moveToFloor(5);
+        assertEquals(5, elevator.getCurrentFloor());
+    }
+
+    @Test
+    void testAddPassenger() {
+        Elevator elevator = new Elevator(0, 10, 10);
+        elevator.addPassenger();
+        assertEquals(1, elevator.getPassengerCount());
+    }
+
+    @Test
+    void testRemovePassenger() {
+        Elevator elevator = new Elevator(0, 10, 10);
+        elevator.addPassenger();
+        elevator.addPassenger();
+        elevator.removePassenger();
+        assertEquals(1, elevator.getPassengerCount());
+    }
+
+    @Test
+    void testMoveToUnreachableFloor() {
+        Elevator elevator = new Elevator(0, 10, 10);
+        elevator.moveToFloor(1);
+        elevator.moveToFloor(15);
+        assertEquals(1, elevator.getCurrentFloor());
+    }
+
+    @Test
+    void testAddPassengerToFullElevator() {
+        Elevator elevator = new Elevator(0, 10, 1);
+        elevator.addPassenger();
+        elevator.addPassenger();
+        assertEquals(1, elevator.getPassengerCount());
     }
 }
